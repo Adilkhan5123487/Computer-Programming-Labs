b5f7317ef70c3df14ccb4d927467186336aa88e9
Adilkhan5123487 <adilkhan7689tngi@gmail.com>
2026-04-21 22:41:48 +0500

    Add files via upload

---
+++ Lab_6/Tasks/Display_Numbers_1_To_10_For_Loop.cpp
@@ -0,0 +1,14 @@
+// Lab Task 1: Display Numbers 1 to 10 Using for Loop
+// Name: Muhammad Adil
+// Registration Number: BF25NWELE0669
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        cout << i << endl;
+    }
+
+    return 0;
+}
